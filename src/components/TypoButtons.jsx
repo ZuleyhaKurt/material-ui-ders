d13b2fabdc7c674664680dfe14d945b0bf890baf
Züleyha Kurt <netice2017@gmail.com>
2022-11-03 20:30:56 +0300

    edit

--- src/components/TypoButtons.jsx
+++ src/components/TypoButtons.jsx
@@ -1,5 +1,15 @@
+import  Typography  from "@mui/material/Typography";
+
 const TypoButtons = () => {
-  return <div>TypoButtons</div>;
+  return <div>
+    <Typography variant="h1" component="h6" align="center" mt={4} color="secondary">
+      MUI typgraphy
+    </Typography>
+
+    <Typography variant="body"   mt={4} align="justify">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti quia iste sed commodi nihil nesciunt exercitationem necessitatibus eveniet vitae ad.
+    </Typography>
+    </div>;
 };
 
 export default TypoButtons;
